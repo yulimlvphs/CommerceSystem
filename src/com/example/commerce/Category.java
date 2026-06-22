22f42fa904c3482ec5bbc1af911ec8f37d9780ff
yulim <iojl1202@naver.com>
2026-06-22 15:12:36 +0900

    feat: LV 2.관리자 모드를 위한 Category 클래스 재정의

--- src/com/example/commerce/Category.java
+++ src/com/example/commerce/Category.java
@@ -6,10 +6,37 @@ public class Category {
     private String categoryName;
     private List<Product> products;
 
-    public Category(String categoryName,
-                    List<Product> products) {
+    public Category(String categoryName, List<Product> products) {
         this.categoryName = categoryName;
         this.products = products;
+    }
+
+    public void addProduct(Product product) {
+        if(hasProductName(product.getName())) {
+            throw new IllegalArgumentException("이미 존재하는 상품입니다. ");
+        }
+    }
+
+    public boolean hasProductName(String productName) {
+        for (Product product : products) {
+            if (product.getName().equals(productName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Product findProductByName(String productName) {
+        for (Product product : products) {
+            if (product.getName().equals(productName)) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public void removeProduct(Product product) {
+        products.remove(product);
     }
 
     public String getCategoryName() {
